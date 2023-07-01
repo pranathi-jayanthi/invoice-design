9fcc5c21707284bb5bd369a743249942f0d367ef
Pranathi-Jayanthi <113671307+Pranathi-Jayanthi@users.noreply.github.com>
2023-07-01 21:55:01 +0530

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div className='App'>
+			<header className='App-header'>
+				<h1>SERVICE ESTIMATION</h1>
+				<h2>CUSTOMER NAME</h2>
+				<p>RAMESH BABU</p>
+				<h2>PHONE NUMBER</h2>
+				<p>9848120945</p>
+				<h2>EMAIL ID</h2>
+				<p>RAMESHBABU@GMAIL.COM</p>
+				<h2>PRODUCT</h2>
+				<p>EPSON L210</p>
+				<h2>ACCESSORIES GIVEN</h2>
+				<p>PRINTER COVER</p>
+				<h2>PROBLEM REPORTED</h2>
+				<p>WHITE LINES IN THE MIDDLE OF PRINT</p>
+        <br></br>
+				<img src='https://www.techopedia.com/wp-content/uploads/2023/03/aee977ce-f946-4451-8b9e-bba278ba5f13.png'></img>
+        <p>asasdastlas</p>
+				<h1>DBA TECHNOLOGIES</h1>
+				<a href='https://www.dbatechnologies.in/' target='_blank'>
+					dbatechnologies.in
+				</a>
+			</header>
+		</div>
   );
 }
 
